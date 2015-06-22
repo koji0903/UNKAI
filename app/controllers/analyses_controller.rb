0c8e7c0d22b0e3a93347818852c41d8b9771f5ef
Koji HIJIKURO <koji0903@gmail.com>
2015-06-22 12:05:04 +0900

    add analyze data

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -7,9 +7,26 @@ class AnalysesController < ApplicationController
   	@all_temperature = all_Weather_temperature
   	@unkai_temperature = unkai_Weather_temperature
 
-  	# 湿度
+  	# 平均風速
+  	@all_ave_wind_speed = all_Weather_ave_wind_speed
+  	@unkai_ave_wind_speed = unkai_Weather_ave_wind_speed
+
+  	# 平均蒸気圧
+  	@all_ave_stream_pressure = all_Weather_ave_stream_pressure
+  	@unkai_ave_stream_pressure = unkai_Weather_ave_stream_pressure
+
+  	# 平均湿度
   	@all_ave_humidity = all_Weather_ave_humidity
   	@unkai_ave_humidity = unkai_Weather_ave_humidity
+
+  	# 最小相対湿度
+  	@all_min_humidity = all_Weather_min_humidity
+  	@unkai_min_humidity = unkai_Weather_min_humidity
+
+  	# 平均現地気圧
+  	@all_ave_pressure = all_Weather_ave_pressure
+  	@unkai_ave_pressure = unkai_Weather_ave_pressure
+
   end
 
   private
@@ -39,7 +56,29 @@ class AnalysesController < ApplicationController
   	data.sort
   end
 
-  # 全ての日の気象情報（平均湿度）を取得
+  # 全ての日の気象情報（平均風速）を取得
+  def all_Weather_ave_wind_speed
+  	cal_diff(Weather.pluck(:ave_wind_speed))
+  end
+
+  # 雲海出た前日の気象情報（平均風速）を取得
+  def unkai_Weather_ave_wind_speed
+  	# 気象情報を取得
+  	cal_diff(Weather.where(:date => @unkai_pre_days).pluck(:ave_wind_speed))
+  end
+
+  # 全ての日の気象情報（平均蒸気圧）を取得
+  def all_Weather_ave_stream_pressure
+  	cal_diff(Weather.pluck(:ave_stream_pressure))
+  end
+
+  # 雲海出た前日の気象情報（平均蒸気圧）を取得
+  def unkai_Weather_ave_stream_pressure
+  	# 気象情報を取得
+  	cal_diff(Weather.where(:date => @unkai_pre_days).pluck(:ave_stream_pressure))
+  end
+
+   # 全ての日の気象情報（平均湿度）を取得
   def all_Weather_ave_humidity
   	cal_diff(Weather.pluck(:ave_humidity))
   end
@@ -49,6 +88,30 @@ class AnalysesController < ApplicationController
   	# 気象情報を取得
   	cal_diff(Weather.where(:date => @unkai_pre_days).pluck(:ave_humidity))
   end
+
+
+  # 全ての日の気象情報（最小相対湿度）を取得
+  def all_Weather_min_humidity
+  	cal_diff(Weather.pluck(:min_himidity))
+  end
+
+  # 雲海出た前日の気象情報（最小相対湿度）を取得
+  def unkai_Weather_min_humidity
+  	# 気象情報を取得
+  	cal_diff(Weather.where(:date => @unkai_pre_days).pluck(:min_himidity))
+  end
+
+  # 全ての日の気象情報（平均現地気圧）を取得
+  def all_Weather_ave_pressure
+  	cal_diff(Weather.pluck(:ave_pressure))
+  end
+
+  # 雲海出た前日の気象情報（平均現地気圧）を取得
+  def unkai_Weather_ave_pressure
+  	# 気象情報を取得
+  	cal_diff(Weather.where(:date => @unkai_pre_days).pluck(:ave_pressure))
+  end
+
 
   def cal_diff(weather)
   	data = Hash.new
