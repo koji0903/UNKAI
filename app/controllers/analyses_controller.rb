7a29adfc689fa8d69a73ab83904922b8c6e849e4
Koji HIJIKURO <koji0903@gmail.com>
2015-06-22 13:51:51 +0900

    add rate graph

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -6,26 +6,37 @@ class AnalysesController < ApplicationController
   	# 気温差
   	@all_temperature = all_Weather_temperature
   	@unkai_temperature = unkai_Weather_temperature
+  	@rate_temperature = cal_rate(@all_temperature,@unkai_temperature)
+
+  	# 降水量
+  	@all_rainfall = all_Weather_rainfall
+  	@unkai_rainfall = unkai_Weather_rainfall
+  	@rate_rainfall = cal_rate(@all_rainfall,@unkai_rainfall)
 
   	# 平均風速
   	@all_ave_wind_speed = all_Weather_ave_wind_speed
   	@unkai_ave_wind_speed = unkai_Weather_ave_wind_speed
+  	@rate_ave_wind_speed = cal_rate(@all_ave_wind_speed,@unkai_ave_wind_speed)
 
   	# 平均蒸気圧
   	@all_ave_stream_pressure = all_Weather_ave_stream_pressure
   	@unkai_ave_stream_pressure = unkai_Weather_ave_stream_pressure
+  	@rate_ave_stream_pressure = cal_rate(@all_ave_stream_pressure,@unkai_ave_stream_pressure)
 
   	# 平均湿度
   	@all_ave_humidity = all_Weather_ave_humidity
   	@unkai_ave_humidity = unkai_Weather_ave_humidity
+  	@rate_ave_humidity = cal_rate(@all_ave_humidity,@unkai_ave_humidity)
 
   	# 最小相対湿度
   	@all_min_humidity = all_Weather_min_humidity
   	@unkai_min_humidity = unkai_Weather_min_humidity
+  	@rate_min_humidity = cal_rate(@all_min_humidity,@unkai_min_humidity)
 
   	# 平均現地気圧
   	@all_ave_pressure = all_Weather_ave_pressure
   	@unkai_ave_pressure = unkai_Weather_ave_pressure
+  	@rate_ave_pressure = cal_rate(@all_ave_pressure,@unkai_ave_pressure)
 
   end
 
@@ -54,6 +65,17 @@ class AnalysesController < ApplicationController
   		end
   	end
   	data.sort
+  end
+
+  # 全ての日の気象情報（降水量）を取得
+  def all_Weather_rainfall
+  	cal_diff2(Weather.pluck(:rainfall))
+  end
+
+  # 雲海出た前日の気象情報（降水量）を取得
+  def unkai_Weather_rainfall
+  	# 気象情報を取得
+  	cal_diff2(Weather.where(:date => @unkai_pre_days).pluck(:rainfall))
   end
 
   # 全ての日の気象情報（平均風速）を取得
@@ -128,6 +150,41 @@ class AnalysesController < ApplicationController
   	data.sort
   end
 
+  # ひとけた目を丸め
+  def cal_diff2(weather)
+  	data = Hash.new
+  	weather.each do |item|
+  		next if item.nil?
+  		tmp = (item/10).to_i * 10
+  		if data.has_key?(tmp) then
+  			no = data[tmp] + 1
+  			data[tmp] = no
+  		else
+  			data[tmp] = 1
+  		end
+  	end
+  	data.sort
+  end
+
+
+  def cal_rate(all_d,unkai)
+  	data = Array.new
+  	all_d.each do |i|
+  		value = 0
+  		unkai.each do |j|
+  			if i[0] == j[0]
+  				value = j[1]
+  				break
+	  		end
+  		end
+  		if value == 0 || value.nil?
+	  		data << [i[0].to_i,0]
+	  	else
+	  		data << [i[0].to_i,((value.to_f*100)/i[1].to_f).to_i]
+	  	end
+  	end
+  	data
+  end
 
   def get_UnkaiDays
   	# 雲海が出た日を配列で取得
