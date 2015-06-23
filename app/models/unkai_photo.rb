d92f43276ed58aa3e5835bfb72600c0fbc5da73d
Koji HIJIKURO <koji0903@gmail.com>
2015-06-23 12:50:45 +0900

    add next/prev link in Unkai Gallery

--- app/models/unkai_photo.rb
+++ app/models/unkai_photo.rb
@@ -1,3 +1,12 @@
 class UnkaiPhoto < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
+
+	def previous
+		UnkaiPhoto.where("id < ?", self.id).order("id DESC").first
+	end
+
+	def next
+		UnkaiPhoto.where("id > ?", self.id).order("id ASC").first
+	end
+
 end
