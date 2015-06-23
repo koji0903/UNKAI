758bad8f103d864c1417aaeffc75ab9b91f4a4ec
Koji HIJIKURO <koji0903@gmail.com>
2015-06-23 16:55:26 +0900

    add Unkai on/off field on Daily Table

--- app/models/weather_hour.rb
+++ app/models/weather_hour.rb
@@ -4,4 +4,10 @@ class WeatherHour < ActiveRecord::Base
       message: "、dateが同じ組み合わせのレコードが既に存在します。",
       scope: [:date,:date_time]
     }
+
+  def get_Unkai(date)
+  	a = UnkaiPhoto.find_by_date(date)
+  	a.id unless a.nil?
+  end
+
 end
