c30240d9cb4507c12a921e181731ecc5f0671ca1
Koji HIJIKURO <koji0903@gmail.com>
2015-06-29 16:42:14 +0900

    BugFix: Miss-View on Sequential Unkai

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -8,10 +8,19 @@ class Weather < ActiveRecord::Base
 
   def get_Unkai(date)
   	flag = ""
+    # 当日出た
   	a = UnkaiPhoto.find_by_date(date)
-  	flag = a.id.to_i unless a.nil?
+    unless a.nil?
+    	flag = a.id.to_i
+      return flag
+    end
+    # 明日出る
   	a = UnkaiPhoto.find_by_date(date+1)
-  	flag = "明日出る！" unless a.nil?
+    unless a.nil?
+    	flag = "明日出る！"
+      return flag
+    end
+    # 明後日出る
   	a = UnkaiPhoto.find_by_date(date+2)
   	flag = "明後日出る！" unless a.nil?
   	flag
--- app/models/weather_hour.rb
+++ app/models/weather_hour.rb
@@ -8,10 +8,16 @@ class WeatherHour < ActiveRecord::Base
   def get_Unkai(date)
   	flag = ""
   	a = UnkaiPhoto.find_by_date(date)
-  	flag = a.id.to_i unless a.nil?
+    unless a.nil?
+    	flag = a.id.to_i
+      return flag
+    end
   	a = UnkaiPhoto.find_by_date(date+1)
-  	flag = "明日出る！" unless a.nil?
-  	a = UnkaiPhoto.find_by_date(date+2)
+    unless a.nil?
+    	flag = "明日出る！"
+      return flag
+    end
+    a = UnkaiPhoto.find_by_date(date+2)
   	flag = "明後日出る！" unless a.nil?
   	flag
   end
